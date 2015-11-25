5ef414bfa7527c02aaa9b5aa051589b6ba11bede
Sunny Ripert <sunny@sunfox.org>
2015-11-25 17:48:02 +0100

    First commit

---
+++ lib/safe_email_name.rb
@@ -0,0 +1,13 @@
+module SafeEmailName
+  def safe_email_name(name, email)
+    escaped_chars = /"<>/
+    email = email.gsub(escaped_chars, '')
+    name = name.gsub(escaped_chars, '')
+    "#{name} <#{email}>"
+  end
+
+  # Make method available as a class method as well.
+  def self.included(base)
+    base.extend(SafeEmailName)
+  end
+end
